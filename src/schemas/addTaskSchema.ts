eb6249f715cbc9cd1aefcc7136ed469a7c19f731
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2025-07-22 20:42:17 -0600

    Fix(addTaskSchema): Fix string() error message of user field

--- src/schemas/addTaskSchema.ts
+++ src/schemas/addTaskSchema.ts
@@ -6,7 +6,7 @@ export const addTaskSchema = z.object({
         .min(3, "Mínimo 3 caracteres")
         .max(30, "Máximo 30 caracteres"),
     date: z.iso.date("La fecha no es válida"),
-    user: z.string()
+    user: z.string("Contenido inválido")
         .min(1, "El usuario es requerido")
 });
 
